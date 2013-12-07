b5ac3405741b48e33c91be41163a82fad3ed61cd
omniaura <omniaura5@gmail.com>
2013-12-07 15:54:50 -0800

    basic views framework, url routing

--- verdict/views.py
+++ verdict/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic.base import View, TemplateView
 
-# Create your views here.
+
+class FrontPageView(TemplateView):
+
+    template_name = "front.html"
+
+
+class AjaxView(View):
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.is_ajax():
+            return super(AjaxView, self).dispatch(request, *args, **kwargs)
+        else:
+            raise Http404
+
+
+class CheckPhoneNumberView(AjaxView):
+    
+    def post(self, request):
+        pass
