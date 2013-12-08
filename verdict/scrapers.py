ba86014bbaffb0207d1ed6f377f528860411f619
omniaura <omniaura5@gmail.com>
2013-12-08 00:45:30 -0800

    added whocalled.us scraper

--- verdict/scrapers.py
+++ verdict/scrapers.py
@@ -34,10 +34,6 @@ browsers = [
                         "Chrome/31.0.1650.57 Safari/537.36")),
 ] # from http://techblog.willshouse.com/2012/01/03/most-common-user-agents/
 
-googlebot = Browser(system="googlebot", 
-                    user_agent=("Mozilla/5.0 (compatible; Googlebot/2.1; "
-                                "+http://www.google.com/bot.html)"))
-
 def url_to_soup(url):
     request = urllib2.Request(url, headers={'User-Agent': 
                                             random.choice(browsers).user_agent})
@@ -51,29 +47,24 @@ def eight_hundred_notes(q):
     '''always fails silently: will most likely return False if failing'''
     url = "http://800notes.com/Phone.aspx/1-%s-%s-%s" % (q[:3], q[3:6], q[6:])
     soup = url_to_soup(url)
+    # see if the element with id "treeTread" has any contents
     if soup.find(id='treeThread').contents:
         return True
     else:
         return False
 
-def caller_complaints(q):
-    url = "http://www.callercomplaints.com/SearchResult.aspx?Phone=%s-%s-%s" %\
-                                                         (q[:3], q[3:6], q[6:])
-    request = urllib2.Request(url, headers={'User-Agent': googlebot.user_agent})
-    page = urllib2.urlopen(request).read()
-    soup = BeautifulSoup(page)
-    print soup.prettify()
-    try:
-        return int(soup.div(class_='rep_val')[5].string) > 0
-    except TypeError:
-        logger.error("scraper 'caller_complaints' failed to convert <div> "
-                     "value to int.")
-        return False # return something workable in the meantime
-
+def who_called_us(q):
+    url = "http://whocalled.us/lookup/%s" % q
+    soup = url_to_soup(url)
+    # look for an element anywhere on the page with id "calls"
+    if soup.find(id='calls'):
+        return True
+    else:
+        return False
 
 # aggregation
 
-scrapers = [caller_complaints,]
+scrapers = [eight_hundred_notes, who_called_us]
 
 def run(q):
     results = []
